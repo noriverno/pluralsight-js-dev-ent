fc468eb5907f375a181b20cbc0cbfdb1d8c6380c
Noe Rivera Hernandez <nriverah@everis.com>
2018-10-18 15:17:29 -0500

    api

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://stark-plains-11816.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
